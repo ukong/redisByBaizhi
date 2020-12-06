7b433e7713fc73022f2f5d255ed488c7b365f23a
wei198621 <wei198621@126.com>
2020-12-07 00:04:10 +0800

    redis4springboot

---
+++ redis_by_springboot/src/main/java/com/tiza/leo/redis_by_springboot/RedisBySpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.tiza.leo.redis_by_springboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisBySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisBySpringbootApplication.class, args);
+	}
+
+}
